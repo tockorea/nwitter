31bf7aa5df10268244f9b93cbeb84fcffe3eeae6
임채은 <lim.chae.eun@gmail.com>
2020-10-22 11:09:45 +0900

    Updates

---
+++ precache-manifest.1f7d00a1b046adb0016cf3d4a304690b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93695a8f098eeb68d7ec12c0be221301",
+    "url": "/tockorea/nwitter/index.html"
+  },
+  {
+    "revision": "c1854c6be934ebcef0e9",
+    "url": "/tockorea/nwitter/static/css/main.d04817b8.chunk.css"
+  },
+  {
+    "revision": "b988b4e4eb912e82d295",
+    "url": "/tockorea/nwitter/static/js/2.de09db78.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/tockorea/nwitter/static/js/2.de09db78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1854c6be934ebcef0e9",
+    "url": "/tockorea/nwitter/static/js/main.896d98c6.chunk.js"
+  },
+  {
+    "revision": "254f21b160d00d88d5ae",
+    "url": "/tockorea/nwitter/static/js/runtime-main.85b650e6.js"
+  }
+]);
